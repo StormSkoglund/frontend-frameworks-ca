58135875bd4c948fb5b8e09a43d8941e337d1f47
Alex Storm Skoglund <alesko52196@stud.noroff.no>
2024-10-10 13:56:23 +0200

    adds a popup via reactjs-popup library

--- src/components/cart/AddToCart.jsx
+++ src/components/cart/AddToCart.jsx
@@ -1,15 +1,24 @@
+import Popup from "reactjs-popup"
 import { useCart } from "/src/components/cart/CartContext.jsx"
 
 const AddToCart = ({ product }) => {
   const { addToCart } = useCart()
 
   return (
-    <button
-      className=" py-2 bg-teal-700 text-white m-5 w-32 block mx-auto rounded-lg hover:bg-green-800 hover:shadow-slate-600 shadow-md text-center font-semibold"
-      onClick={() => addToCart(product)}
+    <Popup
+      trigger={
+        <button className="py-2 bg-teal-700 text-white m-5 w-32 block mx-auto rounded-lg hover:bg-green-800 hover:shadow-slate-600 shadow-md text-center font-semibold">
+          ADD TO CART
+        </button>
+      }
+      position="right center"
+      onOpen={() => addToCart(product)}
     >
-      ADD TO CART
-    </button>
+      <div className="border-theme1 border-4 rounded-e-lg shadow-lg p-5 bg-white text-sm text-black w-4/5">
+        {product.title} added to cart! Click outside this window to close
+        overlay!
+      </div>
+    </Popup>
   )
 }
 
